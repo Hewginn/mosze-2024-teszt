be94a093a41c51c43b0b311b79e219d11b8d2e5e
Hewginn <somogyizolt@gmail.com>
2024-09-18 19:06:56 +0200

    hibák kommentelve

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int *b = new int[NELEMENTS]; //rossz változó név
+    std::cout << '1-100 ertekek duplazasa' //nincs ;
+    for (int i = 0;) //rossz ciklus feltétel
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++)
+    for (int i = 0; i; i++)  //rossz ciklus feltétel
     {
-        std::cout << "Ertek:"
+        std::cout << "Ertek:" //nincs ;
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //atlag 0 inicializálása kell
+    for (int i = 0; i < N_ELEMENTS, i++) //rossz ciklus feltétel
     {
-        atlag += b[i]
+        atlag += b[i] //nincs ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
